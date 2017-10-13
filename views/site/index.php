e92b9ebd73985d7ae4120aeb6a7f554d505ca8f1
German Khokhlov <lkg0dzre@gmail.com>
2017-10-13 21:42:00 +0300

    Оформление загаловка

--- views/site/index.php
+++ views/site/index.php
@@ -7,6 +7,13 @@ $this->title = 'Игра «Електрик»';
 
 <style>
 
+    h1 {
+        font-family : "Courier New", sans-serif;
+        color       : white;
+        text-align  : center;
+        margin      : 50px;
+    }
+
     .b-gamefield {
         width         : 500px;
         height        : 500px;
@@ -17,34 +24,79 @@ $this->title = 'Игра «Електрик»';
         padding       : 20px;
     }
 
-    .b-cell {
+    .b-cell-wrap {
         display          : inline-block;
         width            : 80px;
         height           : 80px;
         margin           : 5px;
-        background-color : #009688;
+        text-align       : center;
         vertical-align   : bottom;
-        border-radius: 100%;
+        background-color : #009688;
+        padding          : 10px;
+        border-radius    : 100%;
+        box-shadow       : inset 0px 0px 10px rgba(0, 0, 0, 0.5);
 
+    }
+
+    .b-cell {
+        width              : 60px;
+        height             : 60px;
+        border-radius      : 100%;
+        margin             : auto;
+        cursor             : pointer;
+        opacity            : 0;
+
+        -webkit-transition : opacity .2s linear;
+        -moz-transition    : opacity .2s linear;
+        -o-transition      : opacity .2s linear;
+        transition         : opacity .2s linear;
     }
 
     .b-cell--lit {
-        box-shadow: 0 0 20px yellow;
-        /* Permalink - use to edit and share this gradient: http://colorzilla.com/gradient-editor/#f8ffe8+0,e3f5ab+33,b7df2d+100;Green+3D+%234 */
-        background: #f8ffe8; /* Old browsers */
-        background: -moz-linear-gradient(top, #f8ffe8 0%, #e3f5ab 33%, #b7df2d 100%); /* FF3.6-15 */
-        background: -webkit-linear-gradient(top, #f8ffe8 0%,#e3f5ab 33%,#b7df2d 100%); /* Chrome10-25,Safari5.1-6 */
-        background: linear-gradient(to bottom, #f8ffe8 0%,#e3f5ab 33%,#b7df2d 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
-        filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f8ffe8', endColorstr='#b7df2d',GradientType=0 ); /* IE6-9 */
+        opacity: 1;
+
+        box-shadow : 0 0 20px yellow;
+
+        background : #f8ffe8; /* Old browsers */
+        background : -moz-linear-gradient(135deg, #f8ffe8 0%, #e3f5ab 33%, #b7df2d 100%); /* FF3.6-15 */
+        background : -webkit-linear-gradient(135deg, #f8ffe8 0%, #e3f5ab 33%, #b7df2d 100%); /* Chrome10-25,Safari5.1-6 */
+        background : linear-gradient(135deg, #f8ffe8 0%, #e3f5ab 33%, #b7df2d 100%); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+        filter     : progid:DXImageTransform.Microsoft.gradient(startColorstr='#f8ffe8', endColorstr='#b7df2d', GradientType=0); /* IE6-9 */
+
+        background-size: 400% 400%;
+
+        -webkit-animation: AnimationName 2s ease infinite;
+        -moz-animation: AnimationName 2s ease infinite;
+        animation: AnimationName 2s ease infinite;
+    }
+
+    .b-cell--hide {
+        opacity: 0;
     }
 
 
 
+    /*** Анимация ***/
 
-
-
+    @-webkit-keyframes AnimationName {
+        0%{background-position:0% 51%}
+        50%{background-position:100% 50%}
+        100%{background-position:0% 51%}
     }
+    @-moz-keyframes AnimationName {
+        0%{background-position:0% 51%}
+        50%{background-position:100% 50%}
+        100%{background-position:0% 51%}
+    }
+    @keyframes AnimationName {
+        0%{background-position:0% 51%}
+        50%{background-position:100% 50%}
+        100%{background-position:0% 51%}
+    }
+
 </style>
+
+<h1><?=$this->title ?></h1>
 
 <div class="site-index">
 
@@ -55,9 +107,9 @@ $this->title = 'Игра «Електрик»';
             <?php for($row=1; $row <= 5; $row++): ?>
                 <?php for($column=1; $column <= 5; $column++): ?>
 
-                    <?php $light_class = ($row > 4 and $column == 3) ? "b-cell--lit" : "" ?>
+                    <?php $light_class = ($row > 3 and $column >= 3) ? "b-cell--lit" : "" ?>
 
-                    --><div class="b-cell <?=$light_class ?>"></div><!--
+                    --><div class="b-cell-wrap"><div class="b-cell js-cell <?=$light_class ?>"></div></div><!--
 
                 <?php endfor ?>
             <?php endfor ?>
@@ -66,3 +118,38 @@ $this->title = 'Игра «Електрик»';
 
     </div>
 </div>
+
+<?php
+
+$script = <<<JS
+    $(document).ready(function() {
+        $('body').on('click', '.js-cell', function(event) {
+            // event.preventDefault();
+
+            toggleCell(this);
+        });
+
+        function toggleCell(element) {
+            if ($(element).hasClass('b-cell--lit')) {
+                hideCell(element);    
+            } else {
+                showCell(element);
+            }
+        }
+        
+        function showCell(element) {
+            element.classList.add('b-cell--lit');
+            setTimeout(function(){element.classList.remove('b-cell--hide');}, 210);
+        }
+        
+        function hideCell(element) {
+            element.classList.add('b-cell--hide');
+            setTimeout(function(){element.classList.remove('b-cell--lit');}, 210);
+        }
+    });
+JS;
+
+
+$this->registerJs($script);
+
+?>
